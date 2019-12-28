58f6ba5dfea50fe55d806c19601288d0c1e01e11
javafalcon <javafalcon@163.com>
2019-12-28 15:00:54 +0800

    add net

--- nets.py
+++ nets.py
@@ -52,24 +52,25 @@ def semiSL2Dnet(shape,num_classes):
     
     return model
     
-def semiSL2Dnet2(shape,num_classes):
+def supLearnNet(shape,num_classes):
     l2value = 0.001
     x_input = Input(shape=shape,name="main_input")
-    conv1 = Conv2D(32,3,padding='same',activation="relu",kernel_regularizer=l2(l2value))(x_input) 
+    conv1 = Conv2D(32,(3,1),padding='same',activation="relu",kernel_regularizer=l2(l2value))(x_input) 
     
-    conv2 = Conv2D(64,3,padding='same',activation="relu",kernel_regularizer=l2(l2value))(conv1)               
-      
-    flatten = Flatten()(conv2)
+    conv2 = Conv2D(32,(1,3),padding='same',activation="relu",kernel_regularizer=l2(l2value))(conv1)               
+    
+    conv3 = Conv2D(100,5,padding='same',activation="relu",kernel_regularizer=l2(l2value))(conv2) 
+    
+    pool1 = MaxPooling2D(2)(conv3)
+    flatten = Flatten()(pool1)
             
     dense_1 = Dense(1024, activation='relu')(flatten)
         
-    drop_1 = Dropout(0.5,name="unsupLayer")
-    x_a = drop_1(dense_1)
-    x_b = drop_1(dense_1)
+    drop_1 = Dropout(0.5)(dense_1)
+        
+    out = Dense(num_classes, activation="softmax")(drop_1)
     
-    out = Dense(num_classes, activation="softmax")(x_a)
-    
-    model = Model(inputs=x_input, outputs=[out, x_b])
+    model = Model(inputs=x_input, outputs=out)
     model.summary()
     
     return model
